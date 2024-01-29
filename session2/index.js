135b01b2c43b66684bbae266b4295302a55c6d07
Zak-65 <sidqui.zakaria1@gmail.com>
2024-01-29 14:44:20 +0100

    first-commit

---
+++ session2/index.js
@@ -0,0 +1,25 @@
+let text1="ofppt";
+text1 +=" ";
+let text2="en force";
+let text3=text1+text2;
+console.log(text3)
+
+let x= 1+1;
+let y="5"+1;
+
+console.log(x,y)
+
+let a=10;
+let b="10";
+if(a!==b){
+    console.log("vrai")
+}
+
+let statut="etudiant";
+let age=21;
+if(statut=="etudiant"&&age==21){
+    console.log("treu")
+}
+else{
+    console.log("false")
+}
