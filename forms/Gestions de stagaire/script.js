90ab6fbc3d2f22c3415f0d5809a20a3f20730084
Zak-65 <sidqui.zakaria1@gmail.com>
2024-04-16 21:59:47 +0100

    gestion de stagaire afficher

--- forms/Gestions de stagaire/script.js
+++ forms/Gestions de stagaire/script.js
@@ -1,5 +1,5 @@
-const stagiare = {}
-const stagiares = []
+let stagiare = {};
+const stagiares = [];
 //Declaration Value forms
 const nomComplet = document.getElementById("nomC_inpt");
 const cin = document.getElementById("cin_inpt");
@@ -8,128 +8,148 @@ const matier = document.getElementById("matier_input");
 const numControle = document.getElementById("num_controle_input");
 const note = document.getElementById("note_input");
 const cin_radio = document.getElementById("cin_radio");
-const note_radio = document.getElementById("note_radio")
-const rechercher_cin_input = document.getElementById("rechercher_cin_input")
+const note_radio = document.getElementById("note_radio");
+const rechercher_cin_input = document.getElementById("rechercher_cin_input");
 const rechercher_note_input = document.getElementById("rechercher_note_input");
-const btn_valid_cherche = document.getElementById("btn_valid_cherche")
+const btn_valid_cherche = document.getElementById("btn_valid_cherche");
+const tableau_cherche = document.getElementById("affichage_cherche");
+const tbody = document.querySelector("tbody");
 // Declaration pattern
-
-
 
 // Declaration btn
 const bnt_ajouter = document.getElementById("btn_ajouter");
 const bnt_recherche = document.getElementById("bnt_recherche");
 const btn_Send = document.getElementById("btn_Send");
 // Declaration items
-const form_display = document.getElementById("form_gestion_stagaire")
-const part_recherche=document.getElementById("part_recherche")
+const form_display = document.getElementById("form_gestion_stagaire");
+const part_recherche = document.getElementById("part_recherche");
 
 // function details gestion
 const totalStagiares = document.getElementById("nbr_stagiare_div");
-document.addEventListener("DOMContentLoaded",()=>totalStagiares.innerHTML=stagiares.length)
+document.addEventListener(
+  "DOMContentLoaded",
+  () => (totalStagiares.innerHTML = stagiares.length)
+);
 
 //----------------- Part for ajouter --------------------------
 // function ajouter
-bnt_ajouter.addEventListener("click",()=>{
-    part_recherche.style.display="none"
-    form_display.style.display="flex"
-    bnt_ajouter.style.background="green"
-    bnt_recherche.style.background=""
-})
-// function sund form
-btn_Send.addEventListener("click",()=>{
-    let verification_form = 0
-    if(nomComplet.value!==""){
-        nomComplet.style.border="green solid 2px"
-        verification_form++
-    }
-    else{
-        nomComplet.style.border="red solid 1px"
-    }
-    if(cin.value!==""){
-        cin.style.border="green solid 2px";
-        verification_form++
-    }
-    else{
-        cin.style.border="red solid 1px";
-    }
-    if(age.value!==""){
-        age.style.border="green solid 2px";
-        verification_form++
-    }
-    else{
-        age.style.border="red solid 1px";
-    }
-    if(matier.value!=="Selectioner votre metier"){
-        matier.style.border="green solid 2px";
-        verification_form++
-    }
-    else{
-        matier.style.border="red solid 1px";
-    }
-    if(numControle.value!==""){
-        numControle.style.border="green solid 2px";
-        verification_form++
-    }
-    else{
-        numControle.style.border="red solid 1px";
-    }
-    if(note.value!==""){
-        note.style.border="green solid 2px";
-        verification_form++
-    }
-    else{
-        note.style.border="red solid 1px";
-    }
-    if(verification_form===6){
-        stagiare[(nomComplet.previousElementSibling.textContent).replace(' ','')]=nomComplet.value;
-        stagiare[(cin.previousElementSibling.textContent).replace(' ','')]=cin.value;
-        stagiare[(age.previousElementSibling.textContent).replace(' ','')]=age.value;
-        stagiare[(matier.previousElementSibling.textContent).replace(' ','')]=matier.value;
-        stagiare[(numControle.previousElementSibling.textContent).replace(' ','')]=numControle.value;
-        stagiare[(note.previousElementSibling.textContent).replace(' ','')]=note.value;
-        stagiares.push(stagiare);
-        console.log(stagiare)
-        console.log(stagiares)
-        totalStagiares.innerHTML=stagiares.length
-        console.log("bien ajouter")
-    }else{
-        console.log("non ajouter")
-    }
+bnt_ajouter.addEventListener("click", () => {
+  part_recherche.style.display = "none";
+  form_display.style.display = "flex";
+  bnt_ajouter.style.background = "green";
+  bnt_recherche.style.background = "";
 });
-// --------------------------------------------------------------  
+// function sund form
+btn_Send.addEventListener("click", () => {
+  let verification_form = 0;
+  if (nomComplet.value !== "") {
+    nomComplet.style.border = "green solid 2px";
+    verification_form++;
+  } else {
+    nomComplet.style.border = "red solid 1px";
+  }
+  if (cin.value !== "") {
+    cin.style.border = "green solid 2px";
+    verification_form++;
+  } else {
+    cin.style.border = "red solid 1px";
+  }
+  if (age.value !== "") {
+    age.style.border = "green solid 2px";
+    verification_form++;
+  } else {
+    age.style.border = "red solid 1px";
+  }
+  if (matier.value !== "Selectioner votre metier") {
+    matier.style.border = "green solid 2px";
+    verification_form++;
+  } else {
+    matier.style.border = "red solid 1px";
+  }
+  if (numControle.value !== "") {
+    numControle.style.border = "green solid 2px";
+    verification_form++;
+  } else {
+    numControle.style.border = "red solid 1px";
+  }
+  if (note.value !== "") {
+    note.style.border = "green solid 2px";
+    verification_form++;
+  } else {
+    note.style.border = "red solid 1px";
+  }
+  if (verification_form === 6) {
+    stagiare[nomComplet.previousElementSibling.textContent.replace(" ", "")] =
+      nomComplet.value;
+    stagiare[cin.previousElementSibling.textContent.replace(" ", "")] =
+      cin.value;
+    stagiare[age.previousElementSibling.textContent.replace(" ", "")] =
+      age.value;
+    stagiare[matier.previousElementSibling.textContent.replace(" ", "")] =
+      matier.value;
+    stagiare[numControle.previousElementSibling.textContent.replace(" ", "")] =
+      numControle.value;
+    stagiare[note.previousElementSibling.textContent.replace(" ", "")] =
+      note.value;
+    stagiares.push(stagiare);
+    console.log(stagiare);
+    console.log(stagiares);
+    totalStagiares.innerHTML = stagiares.length;
+    console.log("bien ajouter");
+    stagiare = {};
+  } else {
+    console.log("non ajouter");
+    stagiare = {};
+  }
+});
+// --------------------------------------------------------------
 
 //-------------------------part of Rechercher-----------------
-cin_radio.checked = true
-let radio_selected =rechercher_cin_input
-bnt_recherche.addEventListener("click",()=>{
-    form_display.style.display="none"
-    part_recherche.style.display="block"
-    bnt_recherche.style.background="green"
-    bnt_ajouter.style.background=""
-})
-cin_radio.addEventListener("click",()=>{
-        rechercher_note_input.style.display="none"
-        rechercher_cin_input.style.display="block"
-        radio_selected = rechercher_cin_input
-})
-note_radio.addEventListener("click",()=>{
-        rechercher_note_input.style.display="block"
-        rechercher_cin_input.style.display="none"
-        radio_selected=rechercher_note_input
-})
-btn_valid_cherche.addEventListener("click",()=>{
-    let result
-    if(radio_selected===rechercher_cin_input){
-        result = stagiares.filter(element=>element.cin==radio_selected.value)
-    }
-    if(radio_selected!==rechercher_cin_input){
-        result = stagiares.filter(element=>element.note==radio_selected.value)
-    }
-    if(result==""){
-        console.log("aucun resultat trouvé")
-    }
-    else{
-        console.log(result)
-    }
-})
+cin_radio.checked = true;
+let radio_selected = rechercher_cin_input;
+bnt_recherche.addEventListener("click", () => {
+  form_display.style.display = "none";
+  part_recherche.style.display = "block";
+  bnt_recherche.style.background = "green";
+  bnt_ajouter.style.background = "";
+});
+cin_radio.addEventListener("click", () => {
+  rechercher_note_input.style.display = "none";
+  rechercher_cin_input.style.display = "block";
+  radio_selected = rechercher_cin_input;
+});
+note_radio.addEventListener("click", () => {
+  rechercher_note_input.style.display = "block";
+  rechercher_cin_input.style.display = "none";
+  radio_selected = rechercher_note_input;
+});
+let result;
+btn_valid_cherche.addEventListener("click", () => {
+  if (radio_selected === rechercher_cin_input) {
+    result = stagiares.filter((element) => element.cin == radio_selected.value);
+    console.log("1");
+  }
+  if (radio_selected !== rechercher_cin_input) {
+    result = stagiares.filter(
+      (element) => element.note == radio_selected.value
+    );
+    console.log("2");
+  }
+  if (result != "") {
+    console.log(result);
+  } else {
+    console.log("aucun resultat trouvé");
+    console.table(stagiares);
+  }
+  tableau_cherche.style.display = "block";
+  result.forEach((element) => {
+    tbody.innerHTML += `<tr>
+                                    <td>${element.Nomcomplet}</td>
+                                    <td>${element.cin}</td>
+                                    <td>${element.Age}</td>
+                                    <td>${element.Matier}</td>
+                                    <td>${element.Numerocontrole}</td>
+                                    <td>${element.note}</td>
+                                </tr>`;
+  });
+});
